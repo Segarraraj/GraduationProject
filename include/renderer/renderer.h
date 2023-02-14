1f0174fe059fb42ecd2eb8ce36642b3fb5c59d26
Segarraraj <segarraraj@esat-alumni.com>
2023-02-14 12:35:03 +0100

    Created DX12 swapchain

--- include/renderer/renderer.h
+++ include/renderer/renderer.h
@@ -3,11 +3,12 @@
 
 #include <memory>
 
+struct ID3D12CommandAllocator;
 struct ID3D12CommandQueue;
+struct IDXGISwapChain3;
 struct IDXGIFactory4;
 struct IDXGIAdapter1;
 struct ID3D12Device;
-struct ID3D12CommandAllocator;
 #ifdef DEBUG
 struct ID3D12Debug1;
 struct ID3D12DebugDevice;
@@ -30,18 +31,22 @@ class Renderer {
   int Init(void (*update)());
   void Start() const;
   void Stop();
+  void Resize();
 
  private:
+  static const unsigned int kSwapchainBufferCount = 2;
+
   std::unique_ptr<RR::Window> _window;
 
-  ID3D12CommandAllocator* _command_allocator;
-  ID3D12CommandQueue* _command_queue;
-  IDXGIFactory4* _factory;
-  IDXGIAdapter1* _adapter;
-  ID3D12Device* _device;
+  ID3D12CommandAllocator* _command_allocator = nullptr;
+  ID3D12CommandQueue* _command_queue = nullptr;
+  IDXGISwapChain3* _swapchain = nullptr;
+  IDXGIFactory4* _factory = nullptr;
+  IDXGIAdapter1* _adapter = nullptr;
+  ID3D12Device* _device = nullptr;
  #ifdef DEBUG
-  ID3D12Debug1* _debug_controller;
-  ID3D12DebugDevice* _debug_device;
+  ID3D12Debug1* _debug_controller = nullptr;
+  ID3D12DebugDevice* _debug_device = nullptr;
  #endif
 
   void (*update_)() = nullptr;
--- source/renderer/renderer.cc
+++ source/renderer/renderer.cc
@@ -38,13 +38,15 @@ int RR::Renderer::Init(void (*update)()) {
   _window->Init(GetModuleHandle(NULL), "winclass", "DX12 Graduation Project",
                 WindowProc, this);
 
+  _window->Show(); 
+
   unsigned int factory_flags = 0;
 
   HRESULT result;
 
 #ifdef DEBUG
   result = D3D12GetDebugInterface(IID_PPV_ARGS(&_debug_controller));
-  if (!SUCCEEDED(result)) {
+  if (FAILED(result)) {
     LOG_ERROR("RR", "Couldn't't get debug interface");
     return 1;
   }
@@ -56,7 +58,7 @@ int RR::Renderer::Init(void (*update)()) {
 #endif
 
   result = CreateDXGIFactory2(factory_flags, IID_PPV_ARGS(&_factory));
-  if (!SUCCEEDED(result)) {
+  if (FAILED(result)) {
     LOG_ERROR("RR", "Couldn't create factory");
     return 1;
   }
@@ -95,7 +97,7 @@ int RR::Renderer::Init(void (*update)()) {
   queue_desc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
 
   result = _device->CreateCommandQueue(&queue_desc, IID_PPV_ARGS(&_command_queue));
-  if (!SUCCEEDED(result)) {
+  if (FAILED(result)) {
     LOG_ERROR("RR", "Couldn't create command queue");
     return 1;
   }
@@ -103,13 +105,42 @@ int RR::Renderer::Init(void (*update)()) {
   result = _device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT,
                                            IID_PPV_ARGS(&_command_allocator));
 
+  RECT window_screen_bounds;
+  GetClientRect((HWND)_window->window(), &window_screen_bounds);
+
+  DXGI_SWAP_CHAIN_DESC1 swapchain_desc = {};
+  swapchain_desc.BufferCount = kSwapchainBufferCount;
+  swapchain_desc.Width = window_screen_bounds.right - window_screen_bounds.left;
+  swapchain_desc.Height = window_screen_bounds.bottom - window_screen_bounds.top;
+  swapchain_desc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+  swapchain_desc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+  swapchain_desc.Scaling = DXGI_SCALING_STRETCH;
+  swapchain_desc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+  swapchain_desc.AlphaMode = DXGI_ALPHA_MODE_IGNORE;
+  swapchain_desc.Flags = 0 | DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+  swapchain_desc.SampleDesc.Count = 1;
+  swapchain_desc.SampleDesc.Quality = 0;
+
+  DXGI_SWAP_CHAIN_FULLSCREEN_DESC swapchain_fs_desc = {};
+  swapchain_fs_desc.Windowed = TRUE;
+
+  IDXGISwapChain1* chain = nullptr;
+  result = _factory->CreateSwapChainForHwnd(
+      _command_queue, (HWND)_window->window(), &swapchain_desc, &swapchain_fs_desc,
+      nullptr, &chain);
+
+  result = chain->QueryInterface(__uuidof(IDXGISwapChain3), (void**)&chain);
+  if (FAILED(result)) {
+    LOG_ERROR("RR", "Couldn't create swapchain");
+    return 1;
+  }
+  _swapchain = (IDXGISwapChain3*) chain;
+
   LOG_DEBUG("RR", "Renderer initialized");
   return 0;
 }
 
 void RR::Renderer::Start() const {
-  _window->Show(); 
-
   while (_running) {
     MSG message;
     while (PeekMessage(&message, (HWND)_window->window(), 0, 0, PM_REMOVE)) {
@@ -122,3 +153,5 @@ void RR::Renderer::Start() const {
 }
 
 void RR::Renderer::Stop() { _running = false; }
+
+void RR::Renderer::Resize(){};
